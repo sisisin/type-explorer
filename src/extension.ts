f0cc3ac733beeeae8a53a4541e8611c89914a393
sisisin <azsisisin@gmail.com>
2020-07-26 15:47:20 +0900

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -8,16 +8,16 @@ import { TypeExplorerProvider } from './tree';
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "try-code-ext" is now active!');
+  console.log('Congratulations, your extension "type-explorer" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('try-code-ext.helloWorld', () => {
+  let disposable = vscode.commands.registerCommand('type-explorer.helloWorld', () => {
     // The code you place here will be executed every time your command is executed
 
     // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from try-code-ext!');
+    vscode.window.showInformationMessage('Hello World from type-explorer!');
   });
 
   vscode.window.registerTreeDataProvider('typeExplorer', new TypeExplorerProvider());
