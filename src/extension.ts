bf4487b0414507bef13fbe47ee8be262a207572a
sisisin <azsisisin@gmail.com>
2020-07-26 15:44:04 +0900

    configure eslint

--- src/extension.ts
+++ src/extension.ts
@@ -6,24 +6,23 @@ import { TypeExplorerProvider } from './tree';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "try-code-ext" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "try-code-ext" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('try-code-ext.helloWorld', () => {
+    // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('try-code-ext.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from try-code-ext!');
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World from try-code-ext!');
   });
-  
+
   vscode.window.registerTreeDataProvider('typeExplorer', new TypeExplorerProvider());
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
--- src/tree.ts
+++ src/tree.ts
@@ -8,14 +8,16 @@ export class TypeExplorerProvider implements vscode.TreeDataProvider<Type> {
   getChildren(element?: Type | undefined): vscode.ProviderResult<Type[]> {
     return [
       new Type('Foo', vscode.TreeItemCollapsibleState.Collapsed),
-      new Type('Bar', vscode.TreeItemCollapsibleState.Collapsed)
+      new Type('Bar', vscode.TreeItemCollapsibleState.Collapsed),
     ];
   }
-
 }
 
 class Type extends vscode.TreeItem {
-  constructor(public readonly label: string, public readonly collapsibleState: vscode.TreeItemCollapsibleState) {
+  constructor(
+    public readonly label: string,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+  ) {
     super(label, collapsibleState);
   }
 }
